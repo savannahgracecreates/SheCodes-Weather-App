4ae7fc4f34767bc960a17138d72566c2bc70edea
Savannah Grace <151082915+savannahgracecreates@users.noreply.github.com>
2024-01-05 13:00:59 -0500

    imported my code from code sandbox
    resulting in a partially completed weather app with styling.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,56 @@
-alert("hello");
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  let city = searchInputElement.value;
+  cityElement.innerHTML = `${city}`;
+
+  let apiKey = "b325fd32a535o3f0ae8cf4f6t48640ab";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
+
+// new function for week 5 challenge
+function displayTemperature(response) {
+  let currentTemp = response.data.temperature.current;
+  console.log(`${currentTemp}`);
+  let displayTemp = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  displayTemp.innerHTML = Math.round(`${currentTemp}`);
+}
